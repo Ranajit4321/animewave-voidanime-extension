25227c3b06a6a2e44aa90c9ea1abe6462c9b9cc4
Ranajit4321 <157198791+Ranajit4321@users.noreply.github.com>
2026-03-13 18:18:13 +0530

    provider.ts

--- animewave.js
+++ animewave.js
@@ -1,16 +1,5 @@
 export default {
+  id: "animewave",
   name: "AnimeWave",
-  baseUrl: "https://animewave.to",
-
-  async search(query) {
-    return `${this.baseUrl}/search?keyword=${query}`;
-  },
-
-  async getAnime(id) {
-    return `${this.baseUrl}/anime/${id}`;
-  },
-
-  async getEpisode(id) {
-    return `${this.baseUrl}/watch/${id}`;
-  }
+  baseUrl: "https://animewave.to"
 };
